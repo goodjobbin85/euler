006ea6385de91454e53eafca40a8ee1af6c35943
Thomas Dubiel <timidturtle85@gmail.com>
2017-08-27 21:04:18 -0500

    final change to assignment

--- euler.rb
+++ euler.rb
@@ -9,12 +9,10 @@ three_count = []
 while ans < 1000
 	ans = a * multiplier
 	multiplier += 1
-	if ans > 1000
+	if ans >= 1000
 		break
 	end
 	three_count << ans
-
-	
 end
 
 print three_count
@@ -27,10 +25,10 @@ second_multiplier = 1
 answer = c
 five_count = []
 
-while  answer < 1000
+while answer < 1000
 	answer = c * second_multiplier
 	second_multiplier += 1
-	if answer > 1000
+	if answer >= 1000
 		break 
 	end
 	five_count << answer
@@ -43,9 +41,15 @@ puts ' '
 
 five_count.each do |number|
 	if three_count.include? number
+		#five_count.delete number
+		number
+	else 
 		three_count << number
 	end
 end
+
+
+puts three_count
 
 full_count = three_count
 
@@ -54,7 +58,8 @@ full_count.each  do |number|
 	sum += number
 end
 
-puts sum
+puts "The sum is #{sum}"
+
 
 
 
